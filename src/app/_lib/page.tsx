9e8bb3945c1cfb16295c67d5be3edff56ee2c894
Sujith P <sujithp2690@gmail.com>
2025-06-10 17:36:26 +0530

    private route like folder name start with _ and name its used for fun path

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function privateRoute() {
+    return (
+        <div>You cant see this in browser this is private route</div>
+    )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChartPage from './chart'
+
+
+function page() {
+    return (
+        <div>
+            dashboard
+            <ChartPage />
+        </div>
+    )
+}
+
+export default page
